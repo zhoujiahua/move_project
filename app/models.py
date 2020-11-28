c9f93ddac3c2940f5e27a8e19ab41dc183cb59ab
Jerry <jiahuasir@gmail.com>
2020-11-29 01:04:27 +0800

    create data tables

--- app/models.py
+++ app/models.py
@@ -2,14 +2,182 @@
 # coding:utf8
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import pymysql
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URL"] = "mysql://root:123456@127.0.0.1:3306/move"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:123456@192.168.0.5:3306/move"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-
 db = SQLAlchemy(app)
 
 
 # 会员
 class User(db.Model):
-    pass
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    pwd = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    phone = db.Column(db.String(11), unique=True)
+    info = db.Column(db.Text)
+    face = db.Column(db.String(255), unique=True)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    uuid = db.Column(db.String(255), unique=True)
+    userlogs = db.relationship("Userlog", backref="user")
+    comments = db.relationship("Comment", backref="user")
+    moviecols = db.relationship("Moviecol", backref="user")
+
+    def __repr__(self):
+        return "<User %>" % self.name
+
+
+# 会员登录
+class Userlog(db.Model):
+    __tablename__ = "userlog"
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    ip = db.Column(db.String(100))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Userlog %>" % self.id
+
+
+# 标签
+class Tag(db.Model):
+    __tablename__ = 'tag'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Tag %>" % self.name
+
+
+# 电影
+class Movie(db.Model):
+    __tablename__ = "movie"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), unique=True)
+    url = db.Column(db.String(255), unique=True)
+    info = db.Column(db.Text)
+    logo = db.Column(db.String(255), unique=True)
+    star = db.Column(db.SmallInteger)
+    playnum = db.Column(db.BigInteger)
+    commentnum = db.Column(db.BigInteger)
+    tag_id = db.Column(db.Integer, db.ForeignKey("tag.id"))
+    area = db.Column(db.String(255))
+    release_time = db.Column(db.Date)
+    length = db.Column(db.String(100))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    comments = db.relationship("Comment", backref="movie")
+    moviecols = db.relationship("Moviecol", backref="movie")
+
+    def __repr__(self):
+        return "<Movie %>" % self.title
+
+
+# 上映预告
+class Preview(db.Model):
+    __tablename__ = "preview"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), unique=True)
+    logo = db.Column(db.String(255), unique=True)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Preview %>" % self.title
+
+
+# 评论
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id = db.Column(db.Integer, primary_key=True)
+    info = db.Column(db.Text)
+    movie_id = db.Column(db.Integer, db.ForeignKey("movie.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Comment %>" % self.id
+
+
+# 收藏
+class Moviecol(db.Model):
+    __tablename__ = 'moviecol'
+    id = db.Column(db.Integer, primary_key=True)
+    movie_id = db.Column(db.Integer, db.ForeignKey("movie.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Moviecol %>" % self.id
+
+
+# 权限
+class Auth(db.Model):
+    __tablename__ = 'auth'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    url = db.Column(db.String(255), unique=True)
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Auth %>" % self.name
+
+
+# 角色
+class Role(db.Model):
+    __tablename__ = 'role'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    auths = db.Column(db.String(600))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Role %>" % self.name
+
+
+# 管理员
+class Admin(db.Model):
+    __tablename__ = 'admin'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    pwd = db.Column(db.String(100), unique=True)
+    is_super = db.Column(db.SmallInteger)
+    role_id = db.Column(db.Integer, db.ForeignKey("role.id"))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    adminlog = db.relationship("Adminlog", backref="admin")
+    oplogs = db.relationship("Oplog", backref="admin")
+
+    def __repr__(self):
+        return "<Admin %>" % self.name
+
+
+# 管理员登录日志
+class Adminlog(db.Model):
+    __tablename__ = 'adminlog'
+    id = db.Column(db.Integer, primary_key=True)
+    admin_id = db.Column(db.Integer, db.ForeignKey("admin.id"))
+    ip = db.Column(db.String(100))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Adminlog %>" % self.id
+
+
+# 操作日志
+class Oplog(db.Model):
+    __tablename__ = 'oplog'
+    id = db.Column(db.Integer, primary_key=True)
+    admin_id = db.Column(db.Integer, db.ForeignKey("admin.id"))
+    ip = db.Column(db.String(100))
+    reason = db.Column(db.String(600))
+    addtime = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return "<Oplog %>" % self.id
+
+
+if __name__ == '__main__':
+    db.create_all()
